950a96f4ec4ce4c9ebd3f8e28fb4023b8a876920
Valentin Mitroi <64599071+valentindaniel9926@users.noreply.github.com>
2021-07-06 11:45:14 +0300

    Add files via upload

---
+++ Threads/src/com/company/AnotherThread.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import static com.company.ThreadColor.ANSI_BLUE;
+
+public class AnotherThread extends  Thread{
+    @Override
+    public void run() {
+        System.out.println(ANSI_BLUE + "Hello from another thread.");
+    }
+}
