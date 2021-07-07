eccecec54172b22634abd15131ac28b922fed08d
Valentin Mitroi <64599071+valentindaniel9926@users.noreply.github.com>
2021-07-07 10:40:17 +0300

    Add files via upload

---
+++ Runnable and Thread/src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import static com.company.ThreadColor.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(ANSI_PURPLE + "Hello from the main thread.");
+        Thread anotherThread = new AnotherThread();
+        anotherThread.setName("== Another Thread ==");
+        anotherThread.start();
+        new Thread() {
+            public void run() {
+                System.out.println(ANSI_GREEN + "Hello from the anonymous class thread.");
+            }
+        }.start();
+        Thread myRunnableThread = new Thread(new MyRunnable(){
+            @Override
+            public void run() {
+                System.out.println(ANSI_CYAN + "Hello from the anonymous class's implementation of run()");
+            }
+        });
+        myRunnableThread.start();
+        System.out.println(ANSI_PURPLE + "Hello again from the main thread");
+
+
+    }
+}
