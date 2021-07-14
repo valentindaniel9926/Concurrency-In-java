e17e86c553bd168ea77913ffd524c8166c9967f1
Valentin Mitroi <64599071+valentindaniel9926@users.noreply.github.com>
2021-07-14 23:06:04 +0300

    Add files via upload

---
+++ Service/src/sample/Controller.java
@@ -0,0 +1,44 @@
+package sample;
+
+import javafx.collections.ObservableList;
+import javafx.concurrent.Service;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ProgressBar;
+
+public class Controller {
+
+    @FXML
+    private ListView listView;
+
+    @FXML
+    private ProgressBar progressBar;
+
+    @FXML
+    private Label progressLabel;
+
+    private Service<ObservableList<String>> service;
+
+    public void initialize() {
+
+        service = new EmployeeService();
+        progressBar.progressProperty().bind(service.progressProperty());
+        progressLabel.textProperty().bind(service.messageProperty());
+        listView.itemsProperty().bind(service.valueProperty());
+
+
+        progressBar.visibleProperty().bind(service.runningProperty());
+        progressLabel.visibleProperty().bind(service.runningProperty());
+    }
+
+    @FXML
+    public void buttonPressed() {
+        if(service.getState() == Service.State.SUCCEEDED) {
+            service.reset();
+            service.start();
+        } else if(service.getState() == Service.State.READY) {
+            service.start();
+        }
+    }
+}
